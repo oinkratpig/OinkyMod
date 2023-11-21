6d60e8b12945dc83249fef83dbae76341edb3c3f
Oinky <contact.knilax@gmail.com>
2023-11-21 16:49:35 -0600

    Better build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,15 @@
+@ECHO off
+
+set "modname=OinkyMod"
+set "fromrelativepath=bin\Debug\netstandard2.1"
+set "destination=C:\Program Files (x86)\Steam\steamapps\common\Lethal Company\BepInEx\plugins"
+
 dotnet build
+
+echo:
+@echo Copying mod files...
+copy "%~dp0\%fromrelativepath%\%modname%.dll" "%destination%"
+@echo Done!
+
+echo:
 pause
